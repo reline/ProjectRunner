4ae4000a77402d72f41092b3146f2ac55afe40d0
Delamare2112 <baladash@localhost.localdomain>
2015-10-12 17:15:20 -0700

    code cleanup

--- jni/src/ProjectRunner.cpp
+++ jni/src/ProjectRunner.cpp
@@ -13,35 +13,10 @@
 #include "Thing.h"
 #include "Vector2.h"
 
-#define SSTR( x ) dynamic_cast< std::ostringstream & >( \
-        ( std::ostringstream() << std::dec << x ) ).str()
-
-//Scene textures
-// LTexture gSplashTexture;
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-bool loadMedia()
-{
-
-	//Load splash texture
-	// if(!gSplashTexture.loadFromFile("52_hello_mobile/hello.bmp"))
-	// {
-	// 	SDL_Log("Failed to load splash texture!\n");
-	// 	return false;
-	// }
-
-	return true;
-}
+#define SSTR(x) dynamic_cast<std::ostringstream&>((std::ostringstream() << std::dec << x)).str()
 
 void close()
 {
-	//Free loaded images
-	// gSplashTexture.free();
 	Game::Exit();
 }
 
@@ -53,51 +28,40 @@ int main( int argc, char* args[] )
 	Thing splash(
 		Transform(Vector2()), 
 		"52_hello_mobile/hello.bmp");
+
 	splash.transform.position = Vector2((Game::instance->screenRect.w - splash.image.getWidth()) / 2, 0);
+	
+	//Main loop flag
+	bool quit = false;
 
-	SDL_Log(("DEBUG: " + SSTR((Game::instance->screenRect.w - splash.image.getWidth()) / 2)).c_str());
-	SDL_Log(("DEBUG: " + SSTR((Game::instance->screenRect.h - splash.image.getHeight()) / 2)).c_str());
+	//Event handler
+	SDL_Event e;
 
-	//Load media
-	if( !loadMedia() )
+	//While application is running
+	while(!quit)
 	{
-		SDL_Log("Failed to load media!\n");
-	}
-	else
-	{
-		//Main loop flag
-		bool quit = false;
-
-		//Event handler
-		SDL_Event e;
-
-		//While application is running
-		while(!quit)
+		//Handle events on queue
+		while( SDL_PollEvent( &e ) != 0 )
 		{
-			//Handle events on queue
-			while( SDL_PollEvent( &e ) != 0 )
+			//User requests quit
+			if( e.type == SDL_QUIT )
 			{
-				//User requests quit
-				if( e.type == SDL_QUIT )
-				{
-					quit = true;
-				}
+				quit = true;
 			}
-
-			splash.transform.position.y++;
-
-			//Clear screen
-			SDL_SetRenderDrawColor( Game::instance->renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-			SDL_RenderClear( Game::instance->renderer );
-
-			//Render splash
-
-			splash.Render();
-			// gSplashTexture.render((Game::instance->screenRect.w - gSplashTexture.getWidth() ) / 2, ( Game::instance->screenRect.h - gSplashTexture.getHeight() ) / 2 );
-
-			//Update screen
-			SDL_RenderPresent(Game::instance->renderer);
 		}
+
+		splash.transform.position.y++;
+
+		//Clear screen
+		SDL_SetRenderDrawColor( Game::instance->renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+		SDL_RenderClear( Game::instance->renderer );
+
+		//Render splash
+
+		splash.Render();
+
+		//Update screen
+		SDL_RenderPresent(Game::instance->renderer);
 	}
 
 	//Free resources and close SDL
